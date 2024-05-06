f364ff4c81e4cec04ac2452a337fdb7ca98952aa
Harsha <164939644+Harshi0004@users.noreply.github.com>
2024-05-06 07:38:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ load_dotenv("config.env")
 
 class Telegram:
     API_ID = int(os.getenv('API_ID', '29982493'))
-    API_HASH = os.getenv('API_HASH', ' 3a64a81221c7e35ae16bfc3f236fe4fe')
+    API_HASH = os.getenv('API_HASH', '3a64a81221c7e35ae16bfc3f236fe4fe')
     BOT_TOKEN = os.getenv('BOT_TOKEN', "7161102507:AAE3fnLw1sfRvuj7IGHS26cfVkpxdgQ1n9M")
     SESSION_STRING = os.getenv('SESSION_STRING', "AQA-i3IABAbGNzHfamlKZnq8weutVtjcP5OSg5a_lr68q2Q7PXZaYKtc0AYStaw_SqOuce4vKNDN3mKqCRKsvbVV3lFWFz5szPM2CYxEmxBGfI0cdK2XjOhcHwBtT8Q0VLM9ziyPETWby3wyvxyM3R7AO17fh6syRh8wOj30Jqs-VVhO1aR4HTNZvleDKKMHRpmMOnDKWRz-sEUo_R8NFiifraJNVJScz7-kp1qnAIOxLzR1taODCMiHev-a80AryLmKTZYd1Y_fyCIwlGe3bmBSwJccagnp2cpFbejIGfXvJoX6-GDI9t3m3AWpRPUE1ddUoIr2PEx_NeE9hk90OOq6UOqC3wAAAAFFv7YAAA")
     PORT = os.getenv("PORT", "8080")
@@ -16,4 +16,4 @@ class Telegram:
     PASSWORD = os.getenv("PASSWORD", "admin")
     SLEEP_THRESHOLD = int(os.getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(os.getenv('WORKERS', '10'))
-    MULTI_CLIENT = bool(os.getenv('MULTI_CLIENT', 'False'))
+    MULTI_CLIENT = os.getenv('MULTI_CLIENT', 'False').lower() == 'true'
